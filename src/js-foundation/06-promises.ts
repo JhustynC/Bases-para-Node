05e316ad1c5e6ae6f92b496aae75829bba35e9cb
JhustynC <christopherarias2002@gmail.com>
2024-08-13 19:30:37 -0500

    Promises Testing

--- src/js-foundation/06-promises.ts
+++ src/js-foundation/06-promises.ts
@@ -1,17 +1,16 @@
-import { http } from '../plugins/index';
+import { http } from "../plugins/index";
 
-export const getPokemonByID = async (id: number | string) : Promise<string> => {
+export const getPokemonByID = async (id: number | string): Promise<string> => {
+  try {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-    
     const pokemon = await http.get(url);
-
-    // throw new Error('Pokemon Undefined')
-    
     return pokemon.name;
-    
-    // return fetch(url)
-    //     .then((response) => response.json())
-    //     // .then(() => {throw new Error('Pokemon Undefined')})
-    //     .then((pokemon) => pokemon.name);
-}
+  } catch (error) {
+    throw new Error(`Pokemon not found with id ${id}`);
+  }
 
+  // return fetch(url)
+  //     .then((response) => response.json())
+  //     // .then(() => {throw new Error('Pokemon Undefined')})
+  //     .then((pokemon) => pokemon.name);
+};
--- test/js-foundation/06-promises.test.ts
+++ test/js-foundation/06-promises.test.ts
@@ -1,10 +1,20 @@
+import { log } from "console";
 import { getPokemonByID } from "./../../src/js-foundation/06-promises";
 
 describe("js-foundation/06-promises", () => {
   test("getPokemonById should be return a pokemon name", async () => {
     const id = 1;
-    const pokemon = await getPokemonByID(id);
+    const pokemonName = await getPokemonByID(id);
     // console.log(pokemon);
-    expect(pokemon).toBe("bulbasaur");
+    expect(pokemonName).toBe("bulbasaur");
+  });
+
+  test("Should return an error if pokemon does not exist", async () => {
+    expect.assertions(1);
+    const id = -1;
+
+    await expect(getPokemonByID(id)).rejects.toThrow(
+      `Pokemon not found with id ${id}`
+    );
   });
 });
